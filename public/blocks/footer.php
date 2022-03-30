fbf6ea4514f39004584ea3db9afe6cba95f96581
Tetyana Pshenyshna <81247244+krypttm@users.noreply.github.com>
2022-03-30 15:44:53 +0300

    Add files via upload

---
+++ public/blocks/footer.php
@@ -0,0 +1,18 @@
+<footer class="container">
+    <div>Все права защищены 2022 &copy;</div>
+    <div class="social">
+
+        <a href="https://t.me/krypptm">
+            <i class="fab fa-telegram"></i>
+        </a>
+
+        <a href="https://github.com/krypttm?tab=repositories">
+            <i class="fab fa-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/%D1%82%D0%B0%D1%82%D1%8C%D1%8F%D0%BD%D0%B0-%D0%BF%D1%88%D0%B5%D0%BD%D0%B8%D1%88%D0%BD%D0%B0-4a17b4230/">
+            <i class="fab fa-linkedin"></i>
+        </a>
+
+    </div>
+</footer>
