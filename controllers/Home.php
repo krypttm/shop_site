edceebef70fd374f8826b51c5c15771b320d0920
Tetyana Pshenyshna <81247244+krypttm@users.noreply.github.com>
2022-03-30 15:09:27 +0300

    Delete controllers directory

--- controllers/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Home extends Controller {
-        public function index() {
-            $products = $this->model('Products');
-
-            $this->view('home/index', $products->getProductsLimited("id", 5));
-            //5 это максимально число записей которое мы хотим вывести
-        }
-    }
--- controllers/Product.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Product extends Controller {
-    public function index($id) {
-        $product = $this->model('Products');
-        $this->view('product/index', $product->getOneProduct($id));
-    }
-}
